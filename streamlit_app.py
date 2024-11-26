596d836e7a77990511e1d0e648b7f6079c673c62
RAFIIF <rafiiffadlurrohman99@gmail.com>
2024-11-26 16:17:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
 st.title("Menghitung :blue[Volume Tabung] :rocket:")
+
+r = st.number_input("Masukkan Jari-jari (cm): ",0)
+r = st.number_input("Masukkan Tinggi (cm): ",0)
+
+if st.button("Hitung Volume", type="primary"):
+  v = math.pi*(r**2)*t
+  st.success(f"Volume tabung adalah {v:.2f}")
